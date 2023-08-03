5159422741b9686766aa3529ce5605e615e109f5
Andrew <andrew@eseeds.com>
2023-08-03 16:08:53 +0100

    Added input function

--- todo/src/Components/Input.js
+++ todo/src/Components/Input.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Input() {
+  const [newTask, setNewTask] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
   }
 
+  function handleChange(e) {
+    console.log(e.target.value);
+    setNewTask(e.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
-        New Task: <input type='text' name='newTask' />
+        New Task: <input type='text' name='newTask' onChange={handleChange} />
       </label>
-      <input type='submit' value='submit' />
+      <input type='button' value='Submit' />
     </form>
   );
 }
